bc5753334d083b7a50fdde75ea43ef1e95193c63
Thomas Thurman <thomas@thurman.org.uk>
2018-04-24 13:37:29 +0100

    _get_json method to make test failures a bit less cryptic

--- elephant/tests.py
+++ elephant/tests.py
@@ -19,12 +19,21 @@ TOKEN_REQUEST_PARAMS = {
         'scope': 'read write follow',
         }
 
-class AuthTests(TestCase):
+class UnChapeauTestCase(TestCase):
+
+    def _get_json(self, response):
+        self.assertEqual(response.status_code, 200,
+                msg=str(response.content))
+        self.assertEqual(response['Content-Type'], 'application/json',
+                msg=str(response.content))
+        return response.json()
+
+class AuthTests(UnChapeauTestCase):
     def test_create_app(self):
         c = Client()
 
-        app = c.post('/api/v1/apps',
-                APPS_CREATE_PARAMS).json()
+        app = self._get_json(c.post('/api/v1/apps',
+                APPS_CREATE_PARAMS))
 
         for key in ['client_id', 'client_secret', 'id']:
             self.assertIn(key, app)
@@ -32,8 +41,8 @@ class AuthTests(TestCase):
     def test_create_token(self):
         c = Client()
 
-        app = c.post('/api/v1/apps',
-                APPS_CREATE_PARAMS).json()
+        app = self._get_json(c.post('/api/v1/apps',
+                APPS_CREATE_PARAMS))
 
         token_params = TOKEN_REQUEST_PARAMS
         self.user = User.objects.create_user(
@@ -44,8 +53,8 @@ class AuthTests(TestCase):
         for key in ['client_id', 'client_secret']:
             token_params[key] = app[key]
 
-        token = c.post('/oauth/token',
-                TOKEN_REQUEST_PARAMS).json()
+        token = self._get_json(c.post('/oauth/token',
+                TOKEN_REQUEST_PARAMS))
 
         for key in ['access_token','token_type','scope']:
             self.assertIn(key, token)
@@ -56,8 +65,8 @@ class AuthTests(TestCase):
     def test_login(self):
         c = Client()
 
-        app = c.post('/api/v1/apps',
-                APPS_CREATE_PARAMS).json()
+        app = self._get_json(c.post('/api/v1/apps',
+                APPS_CREATE_PARAMS))
 
         token_params = TOKEN_REQUEST_PARAMS
         self.user = User.objects.create_user(
@@ -68,11 +77,11 @@ class AuthTests(TestCase):
         for key in ['client_id', 'client_secret']:
             token_params[key] = app[key]
 
-        token = c.post('/oauth/token',
-                TOKEN_REQUEST_PARAMS).json()
+        token = self._get_json(c.post('/oauth/token',
+                TOKEN_REQUEST_PARAMS))
 
-        account = c.get('/api/v1/accounts/verify_credentials',
-                HTTP_AUTHORIZATION = 'Bearer '+token['access_token']).json()
+        account = self._get_json(c.get('/api/v1/accounts/verify_credentials',
+                HTTP_AUTHORIZATION = 'Bearer '+token['access_token']))
 
         for key in ['id', 'username', 'acct', 'display_name',
                 'locked', 'created_at', 'note', 'avatar',
