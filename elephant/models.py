570b8f4b4eb612e7f74332eed107d78fd10c02cd
Thomas Thurman <thomas@thurman.org.uk>
2018-04-28 17:16:23 +0100

    JSON representations moved from view to model; status req includes proper account information

--- elephant/models.py
+++ elephant/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.timezone import now
 
+def iso_date(date):
+    return date.isoformat()+'Z'
+
 class User(AbstractUser):
 
     REQUIRED_FIELDS = ['username']
@@ -31,6 +34,37 @@ class User(AbstractUser):
 
     moved_to = models.CharField(max_length=255,
             default='')
+
+
+    def as_json(self,
+            include_source=False):
+
+        result = {
+            'id': self.id,
+            'username': self.username,
+            'acct': self.username, # XXX for remote ones we need to split this up
+            'display_name': self.display_name,
+            'locked': self.is_locked,
+            'created_at': iso_date(self.created_at),
+            'note': self.note,
+            'url': self.url,
+            'avatar': '/static/un_chapeau/defaults/avatar_1.jpg',
+            'avatar_static': '/static/un_chapeau/defaults/avatar_1.jpg',
+            'header': '/static/un_chapeau/defaults/header.jpg',
+            'header_static': '/static/un_chapeau/defaults/header.jpg',
+            'followers_count': 0,
+            'following_count': 0,
+            'statuses_count': 0,
+        }
+
+        if include_source:
+            result['source'] = {
+                'privacy': 'public',
+                'sensitive': False,
+                'note': self.note,
+                }
+
+        return result
 
 class Status(models.Model):
 
@@ -85,3 +119,38 @@ class Status(models.Model):
 
     def __str__(self):
         return str(self.user) + " - " + self.content
+
+    def as_json(self):
+
+        result = {
+                "id": self.posted_by().as_json,
+                "uri": "?", # https://toot.love/users/un_chapeau_test/statuses/99927189452438245
+                "url": "?", # https://toot.love/@un_chapeau_test/99927189452438245
+                "account": "your account",
+                "content": self.content,
+                'created_at': iso_date(self.created_at),
+                "emojis": [],
+                "reblogs_count": 0,
+                "favourites_count": 0,
+                "reblogged": False,
+                "favourited": False,
+                "muted": False,
+                "sensitive": self.is_sensitive(),
+                "spoiler_text": self.spoiler_text,
+                "visibility": self.visibility,
+                "media_attachments": [],
+                "mentions": [],
+                "tags": [],
+                "language": '', # XXX null?
+                "pinned": False,
+                # XXX in_reply_to_id
+                # XXX in_reply_to_account_id
+                # XXX application
+            }
+
+# XXX Need to wrap oauth2's Application. For now:
+def application_as_json(app):
+    return {
+            "name": app.name,
+            "website": app.website,
+            }
