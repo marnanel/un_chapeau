dea177ac5e09db2ec6ea8fad6ec2393f7ee431fc
Marnanel Thurman <marnanel@thurman.org.uk>
2018-07-18 14:02:21 +0100

    Removed all trailing "/"s from url paths.

--- un_chapeau/urls.py
+++ un_chapeau/urls.py
@@ -13,7 +13,7 @@ import un_chapeau.settings as settings
 # OAuth2 provider endpoints
 
 oauth2_endpoint_views = [
-    path('authorize/', oauth2_views.AuthorizationView.as_view(), name="authorize"),
+    path('authorize', oauth2_views.AuthorizationView.as_view(), name="authorize"),
     path('token', oauth2_views.TokenView.as_view(), name="token"),
     path('revoke-token', oauth2_views.RevokeTokenView.as_view(), name="revoke-token"),
 ]
@@ -21,14 +21,14 @@ oauth2_endpoint_views = [
 ##################################################
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
 
-    path('accounts/login/', auth_views.login, name='login'),
-    path('accounts/logout/', auth_views.logout, name='logout'),
+    path('accounts/login', auth_views.login, name='login'),
+    path('accounts/logout', auth_views.logout, name='logout'),
 
-    path('oauth/', include((oauth2_endpoint_views, 'oauth2_provider'), namespace="oauth2_provider")),
+    path('oauth', include((oauth2_endpoint_views, 'oauth2_provider'), namespace="oauth2_provider")),
 
-    path('api/', include((trilby_api.urls.endpoints, 'trilby_api'), namespace="trilby_api")),
+    path('api', include((trilby_api.urls.endpoints, 'trilby_api'), namespace="trilby_api")),
 
     path('users/<username>/feed', trilby_api.views.UserFeed.as_view()),
     path('.well-known/webfinger', trilby_api.views.Webfinger.as_view()),
@@ -38,7 +38,7 @@ urlpatterns = [
 
     path('', tophat_ui.views.FrontPage.as_view()),
     path('about', tophat_ui.views.FrontPage.as_view()),
-    path('users/<username>/', tophat_ui.views.UserPage.as_view()),
+    path('users/<username>', tophat_ui.views.UserPage.as_view()),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
         + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
