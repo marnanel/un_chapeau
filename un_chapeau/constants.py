cf2325dc7390e5e62dbdcf21276a798abd52d549
Marnanel Thurman <marnanel@thurman.org.uk>
2018-07-30 17:27:45 +0100

    Constants needed across the project.

---
+++ un_chapeau/constants.py
@@ -0,0 +1,24 @@
+ATSIGN_CONTEXT = [
+        "https://www.w3.org/ns/activitystreams", 
+        "https://w3id.org/security/v1", 
+        {
+            "schema": "http://schema.org#", 
+            "inReplyToAtomUri": "ostatus:inReplyToAtomUri", 
+            "movedTo": "as:movedTo", 
+            "conversation": "ostatus:conversation", 
+            "ostatus": "http://ostatus.org#", 
+            "atomUri": "ostatus:atomUri", 
+            "featured": "toot:featured", 
+            "value": "schema:value", 
+            "PropertyValue": "schema:PropertyValue", 
+            "sensitive": "as:sensitive", 
+            "toot": "http://joinmastodon.org/ns#", 
+            "Hashtag": "as:Hashtag", 
+            "manuallyApprovesFollowers": "as:manuallyApprovesFollowers", 
+            "focalPoint": {
+                "@id": "toot:focalPoint", 
+                "@container": "@list"
+                }, 
+            "Emoji": "toot:Emoji"
+            }
+        ]
